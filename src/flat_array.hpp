1e2422a3683c4c00a9a6a4bfca1c17e3a818c524
Andreas Schaefer <gentryx@gmx.de>
2013-11-19 17:41:51 +0100

    added const accessor to add const correctness to soa_array

--- src/flat_array.hpp
+++ src/flat_array.hpp
@@ -38,7 +38,10 @@
  */
 #define LIBFLATARRAY_ACCESS(CELL)                                       \
     template<typename CELL_TYPE, int MY_DIM_X, int MY_DIM_Y, int MY_DIM_Z, int INDEX> \
-    friend class LibFlatArray::soa_accessor;
+    friend class LibFlatArray::soa_accessor;                            \
+                                                                        \
+    template<typename CELL_TYPE, int MY_DIM_X, int MY_DIM_Y, int MY_DIM_Z, int INDEX> \
+    friend class LibFlatArray::const_soa_accessor;
 
 namespace LibFlatArray {
 
@@ -66,13 +69,11 @@ class aggregated_member_size;
 template<typename CELL, int DIM_X, int DIM_Y, int DIM_Z, int INDEX>
 class soa_accessor;
 
-/**
- * Instances of this class will be returned by
- * soa_accessor::operator[]. Separation of both is done to reduce
- * compilation times.
- */
+template<typename CELL, int DIM_X, int DIM_Y, int DIM_Z, int INDEX>
+class const_soa_accessor;
+
 template<typename CELL, int GRID_DIM, int INDEX>
-class soa_accessor_final;
+class const_soa_accessor_final;
 
 namespace detail {
 
@@ -80,7 +81,7 @@ namespace flat_array {
 
 /**
  * This helper class is used to retrieve objects from the SoA storage
- * via an accessor.
+ * with the help of an accessor.
  */
 template<typename CELL>
 class get_instance_functor
@@ -497,19 +498,79 @@ class coord
             CELL_MEMBERS);                                              \
                                                                         \
         __host__ __device__                                             \
-            const char *get_data() const                                \
+        const char *get_data() const                                    \
         {                                                               \
             return data;                                                \
         }                                                               \
                                                                         \
         __host__ __device__                                             \
-            char *get_data()                                            \
+        char *get_data()                                                \
         {                                                               \
             return data;                                                \
         }                                                               \
                                                                         \
     private:                                                            \
         char *data;                                                     \
+        int *index;                                                     \
+    };                                                                  \
+                                                                        \
+    template<int MY_DIM_X, int MY_DIM_Y, int MY_DIM_Z, int INDEX>       \
+    class const_soa_accessor<CELL_TYPE, MY_DIM_X, MY_DIM_Y, MY_DIM_Z, INDEX> \
+    {                                                                   \
+    public:                                                             \
+        typedef CELL_TYPE MyCell;                                       \
+                                                                        \
+        static const int DIM_X = MY_DIM_X;                              \
+        static const int DIM_Y = MY_DIM_Y;                              \
+        static const int DIM_Z = MY_DIM_Z;                              \
+                                                                        \
+        __host__ __device__                                             \
+        const_soa_accessor(const char *data=0, int *index=0) :          \
+            data(data),                                                 \
+            index(index)                                                \
+        {}                                                              \
+                                                                        \
+        template<int X, int Y, int Z>                                   \
+        inline                                                          \
+        __host__ __device__                                             \
+        const_soa_accessor<CELL_TYPE, LIBFLATARRAY_PARAMS> operator[](coord<X, Y, Z>) const \
+        {                                                               \
+            return const_soa_accessor<CELL_TYPE, LIBFLATARRAY_PARAMS>(data, index); \
+        }                                                               \
+                                                                        \
+        __host__ __device__                                             \
+        inline                                                          \
+        void operator>>(CELL_TYPE& cell) const                          \
+        {                                                               \
+            BOOST_PP_SEQ_FOR_EACH(                                      \
+                COPY_SOA_MEMBER_OUT,                                    \
+                CELL_TYPE,                                              \
+                CELL_MEMBERS);                                          \
+        }                                                               \
+                                                                        \
+        __host__ __device__                                             \
+        inline                                                          \
+        void save(char *target, size_t count) const                     \
+        {                                                               \
+            BOOST_PP_SEQ_FOR_EACH(                                      \
+                COPY_SOA_MEMBER_ARRAY_OUT,                              \
+                CELL_TYPE,                                              \
+                CELL_MEMBERS);                                          \
+        }                                                               \
+                                                                        \
+        BOOST_PP_SEQ_FOR_EACH(                                          \
+            DECLARE_SOA_MEMBER_CONST,                                   \
+            CELL_TYPE,                                                  \
+            CELL_MEMBERS);                                              \
+                                                                        \
+        __host__ __device__                                             \
+        const char *get_data() const                                    \
+        {                                                               \
+            return data;                                                \
+        }                                                               \
+                                                                        \
+    private:                                                            \
+        const char *data;                                               \
         int *index;                                                     \
     };                                                                  \
                                                                         \
@@ -555,12 +616,26 @@ public:
 
     inline
     __host__ __device__
-    const soa_accessor<CELL, SIZE, 1, 1, 0> operator[](int& index) const
+    const const_soa_accessor<CELL, SIZE, 1, 1, 0> operator[](int& index) const
     {
-        return soa_accessor<CELL, SIZE, 1, 1, 0>(data, &index);
+        return const_soa_accessor<CELL, SIZE, 1, 1, 0>(data, &index);
     }
 
     inline
+    __host__ __device__
+    soa_accessor<CELL, SIZE, 1, 1, 0> at(int& index)
+    {
+        return (*this)[index];
+    }
+
+    inline
+    __host__ __device__
+    const_soa_accessor<CELL, SIZE, 1, 1, 0> at(int& index) const
+    {
+        return (*this)[index];
+    }
+
+   inline
     __host__ __device__
     void operator<<(const CELL& cell)
     {
