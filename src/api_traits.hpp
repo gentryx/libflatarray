c433009c8820f9065e0080c5099b9addb99deec8
Andreas Schaefer <gentryx@gmx.de>
2014-06-24 09:29:40 +0200

    adding api_traits, a tool to let use code configure certain library features

---
+++ src/api_traits.hpp
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2014 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef FLAT_ARRAY_API_TRAITS_HPP
+#define FLAT_ARRAY_API_TRAITS_HPP
+
+#include <libflatarray/soa_accessor.hpp>
+
+namespace LibFlatArray {
+
+/**
+ * This class provides means of describing properties of user classes
+ * and discovering them from library code. The crucial benefit is that
+ * we can define default values if a property is undefined. By this we
+ * can achive forward compatibility of user code.
+ *
+ * This class is equivalent to LibGeoDecomp's APITraits class.
+ */
+class api_traits
+{
+public:
+    class false_type
+    {};
+
+    class true_type
+    {};
+
+    class has_default_2d_sizes
+    {
+    public:
+        typedef void has_sizes;
+    };
+
+    class has_default_3d_sizes
+    {
+    public:
+        typedef void has_sizes;
+
+        template<typename CELL, typename FUNCTOR>
+        void select_size(
+            char *data,
+            FUNCTOR& functor,
+            const std::size_t dim_x = 1,
+            const std::size_t dim_y = 1,
+            const std::size_t dim_z = 1)
+        {
+            bind_size_x<CELL>(dim_x, dim_y, dim_z, data, functor);
+        }
+
+    private:
+        template<typename CELL, typename FUNCTOR>
+        void bind_size_x(
+            const std::size_t dim_x,
+            const std::size_t dim_y,
+            const std::size_t dim_z,
+            char *data,
+            FUNCTOR& functor)
+        {
+#define CASE(SIZE)                                                   \
+            if (dim_x <= SIZE) {                                     \
+                bind_size_z<CELL, SIZE, SIZE>(                       \
+                    dim_z, data, functor);                           \
+                return;                                              \
+            }
+
+            CASE(  32);
+            CASE( 128);
+            CASE( 192);
+            CASE( 256);
+            CASE( 512);
+            CASE( 544);
+            CASE(1056);
+#undef CASE
+            // fixme: add/use bind_size_y()
+        }
+
+        template<typename CELL, int DIM_X, int DIM_Y, typename FUNCTOR>
+        void bind_size_z(const std::size_t dim_z, char *data, FUNCTOR& functor)
+        {
+#define CASE(SIZE)                                                      \
+            if (dim_z <= SIZE) {                                        \
+                soa_accessor<CELL, DIM_X, DIM_Y, SIZE, 0>  accessor(    \
+                    data, 0);                                           \
+                functor(accessor,                                       \
+                        &accessor.index);                               \
+                return;                                                 \
+            }
+
+            CASE(  32);
+            CASE( 256);
+            CASE(1026);
+
+#undef CASE
+        }
+
+    };
+
+    class has_custom_sizes
+    {
+        typedef void has_sizes;
+    };
+
+    template<typename T, typename HAS_SIZES = void>
+    class select_sizes : public has_default_3d_sizes
+    {};
+
+    template<typename T>
+    class select_sizes<T, typename T::API::has_sizes>
+    {
+    public:
+    };
+
+    class has_asymmetric_dual_callback
+    {};
+
+    class has_no_asymmetric_dual_callback
+    {};
+
+    class select_asymmetric_dual_callback
+    {};
+};
+
+}
+
+#endif
---
+++ test/api_traits_test.cpp
@@ -0,0 +1,110 @@
+/**
+ * Copyright 2014 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/assign/std/vector.hpp>
+#include <boost/detail/lightweight_test.hpp>
+#include <iostream>
+#include <vector>
+#include <libflatarray/api_traits.hpp>
+#include <libflatarray/soa_grid.hpp>
+#include <libflatarray/macros.hpp>
+
+#include "test.h"
+
+class Cell
+{
+public:
+    double memberA;
+    double memberB;
+    double memberC;
+};
+
+LIBFLATARRAY_REGISTER_SOA(Cell, ((double)(memberA))((double)(memberB))((double)(memberC)))
+
+namespace LibFlatArray {
+
+using namespace boost::assign;
+
+class CellDefaultSizes
+{
+public:
+};
+
+// class CellDefault2DSizes
+// {
+// public:
+//     class API : public LibFlatArray::api_traits::has_default_2d_sizes
+//     {};
+// };
+
+class CellDefault3DSizes
+{
+public:
+    class API : public api_traits::has_default_3d_sizes
+    {};
+};
+
+class TestFunctor
+{
+public:
+    TestFunctor(std::vector<int> *report) :
+        report(report)
+    {}
+
+    template<typename CELL, int DIM_X, int DIM_Y, int DIM_Z, int INDEX>
+    void operator()(soa_accessor<CELL, DIM_X, DIM_Y, DIM_Z, INDEX>& accessor, int *unused)
+    {
+        *report += DIM_X, DIM_Y, DIM_Z, INDEX;
+    }
+
+private:
+    std::vector<int> *report;
+};
+
+ADD_TEST(TestSelectSizes)
+{
+    char data[1024 * 1024];
+    std::vector<int> actual;
+    std::vector<int> expected;
+    TestFunctor functor(&actual);
+    typedef api_traits::select_sizes<CellDefaultSizes> selector;
+
+    selector().select_size<Cell>(data, functor, 10, 20, 30);
+    expected += 32, 32, 32, 0;
+    BOOST_TEST(actual == expected);
+    actual.clear();
+    expected.clear();
+
+    selector().select_size<Cell>(data, functor, 30, 30, 30);
+    expected += 32, 32, 32, 0;
+    BOOST_TEST(actual == expected);
+    actual.clear();
+    expected.clear();
+
+    selector().select_size<Cell>(data, functor, 32, 32, 32);
+    expected += 32, 32, 32, 0;
+    BOOST_TEST(actual == expected);
+    actual.clear();
+    expected.clear();
+
+    selector().select_size<Cell>(data, functor, 40, 32, 32);
+    expected += 128, 128, 32, 0;
+    BOOST_TEST(actual == expected);
+    actual.clear();
+    expected.clear();
+
+    // for (int i = 0; i < actual.size(); ++i) {
+    //     std::cout << "actual[" << i << "] = " << actual[i] << "\n";
+    // }
+}
+
+}
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
