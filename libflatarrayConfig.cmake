5b004ffeefe61f6fcdb541375d09ec025ff790cc
Andreas Schaefer <gentryx@gmx.de>
2014-09-05 23:24:17 +0200

    same

--- libflatarrayConfig.cmake
+++ libflatarrayConfig.cmake
@@ -1,3 +1,2 @@
 get_filename_component(LIBFLATARRAY_CMAKE_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-include_directories("${LIBFLATARRAY_CMAKE_DIR}/src")
-message(info "LIBFLATARRAY at ${LIBFLATARRAY_CMAKE_DIR}/src")
+set(libflatarray_INCLUDE_DIR "${LIBFLATARRAY_CMAKE_DIR}/src")
