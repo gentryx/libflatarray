f2459b2879d6b5ca76c98062229c8878cdf3ca1d
Andreas Schaefer <gentryx@gmx.de>
2014-12-01 21:03:34 +0100

    added scalar implementation for short_vec<float, 32>

--- src/short_vec.hpp
+++ src/short_vec.hpp
@@ -22,6 +22,7 @@
 
 #include <libflatarray/detail/short_vec_scalar_float_1.hpp>
 #include <libflatarray/detail/short_vec_scalar_float_2.hpp>
+#include <libflatarray/detail/short_vec_scalar_float_32.hpp>
 
 #include <libflatarray/detail/short_vec_sse_double_2.hpp>
 #include <libflatarray/detail/short_vec_sse_double_4.hpp>
