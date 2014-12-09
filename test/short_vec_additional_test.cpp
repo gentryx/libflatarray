ab8e4f0734595b3d806d1a42bfaa1332fceebac1
Andreas Schaefer <gentryx@gmx.de>
2014-12-09 16:10:01 +0100

    added additional source file to check whether functions are properly inlined

---
+++ test/short_vec_additional_test.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2014 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <libflatarray/short_vec.hpp>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
