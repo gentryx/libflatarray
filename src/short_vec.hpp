a0322387ab11abb8ee2236215cf8b1cbc3b12ebe
Kurt Kanzenbach <kurt@kmk-computers.de>
2015-10-12 14:17:51 +0200

    short_vec: removed duplicated include
    --HG--
    extra : histedit_source : 3e302e99e00759a8f225d2f3f2a15fd349cc7f9d

--- src/short_vec.hpp
+++ src/short_vec.hpp
@@ -53,8 +53,6 @@ public:
 #include <libflatarray/detail/short_vec_avx_float_16.hpp>
 #include <libflatarray/detail/short_vec_avx_float_32.hpp>
 
-#include <libflatarray/detail/short_vec_neon_float_4.hpp>
-
 #include <libflatarray/detail/short_vec_scalar_double_1.hpp>
 #include <libflatarray/detail/short_vec_scalar_double_2.hpp>
 #include <libflatarray/detail/short_vec_scalar_double_4.hpp>
