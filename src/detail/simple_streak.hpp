fa89dffe322cc763d3532fc952df3a17b9197b29
Andreas Schäfer <gentryx@gmx.de>
2016-05-06 21:50:59 +0200

    adding simplistic streak implementation for upcoming change to soa_grid::load()/save()

---
+++ src/detail/simple_streak.hpp
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2016 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef FLAT_ARRAY_DETAIL_SIMPLE_STREAK_HPP
+#define FLAT_ARRAY_DETAIL_SIMPLE_STREAK_HPP
+
+namespace LibFlatArray {
+
+namespace detail {
+
+namespace flat_array {
+
+    class simple_streak {
+    public:
+        simple_streak(std::size_t x = 0, std::size_t y = 0, std::size_t z = 0, std::size_t count = 0) :
+            count(count)
+        {
+            origin[0] = x;
+            origin[1] = y;
+            origin[2] = z;
+        }
+
+        std::size_t length() const
+        {
+            return count;
+        }
+
+        std::size_t origin[3];
+        std::size_t count;
+    };
+
+}
+
+}
+
+}
+
+#endif
