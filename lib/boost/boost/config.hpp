0de6fd610cd32831b06fee9b5c883a240589d030
Andreas Schäfer <gentryx@gmx.de>
2016-06-15 23:50:00 +0200

    still adding boost headers for LGD

--- lib/boost/boost/config.hpp
+++ lib/boost/boost/config.hpp
@@ -1,3 +1,12 @@
+// if we don't have a std library config set, try and find one:
+#if !defined(BOOST_STDLIB_CONFIG) && !defined(BOOST_NO_STDLIB_CONFIG) && !defined(BOOST_NO_CONFIG) && defined(__cplusplus)
+#  include <boost/config/select_stdlib_config.hpp>
+#endif
+// if we have a std library config, include it now:
+#ifdef BOOST_STDLIB_CONFIG
+#  include BOOST_STDLIB_CONFIG
+#endif
+
 // get config suffix code:
 #include <boost/config/suffix.hpp>
 
