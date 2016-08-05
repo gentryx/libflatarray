6087a9b9f1b691bffec7d2602bca047f2b26c9c3
Andreas Schäfer <gentryx@gmx.de>
2016-08-06 01:05:03 +0200

    add conditional macro LIBFLATARRAY_IF_SHORTER to selectively enable code depending on the lenght of a list

--- test/preprocessor_test.cpp
+++ test/preprocessor_test.cpp
@@ -64,7 +64,51 @@ ADD_TEST(TestDequeue)
 
 ADD_TEST(TestIfShorter)
 {
-    // fixme
+    bool a0 = LIBFLATARRAY_IF_SHORTER(LIST_A, 0, false, true);
+    bool a1 = LIBFLATARRAY_IF_SHORTER(LIST_A, 1, true, false);
+    bool a2 = LIBFLATARRAY_IF_SHORTER(LIST_A, 2, true, false);
+    bool a3 = LIBFLATARRAY_IF_SHORTER(LIST_A, 3, true, false);
+    bool a4 = LIBFLATARRAY_IF_SHORTER(LIST_A, 4, true, false);
+
+    bool b0 = LIBFLATARRAY_IF_SHORTER(LIST_B, 0, false, true);
+    bool b1 = LIBFLATARRAY_IF_SHORTER(LIST_B, 1, false, true);
+    bool b2 = LIBFLATARRAY_IF_SHORTER(LIST_B, 2, false, true);
+    bool b3 = LIBFLATARRAY_IF_SHORTER(LIST_B, 3, false, true);
+    bool b4 = LIBFLATARRAY_IF_SHORTER(LIST_B, 4, false, true);
+    bool b5 = LIBFLATARRAY_IF_SHORTER(LIST_B, 5, false, true);
+    bool b6 = LIBFLATARRAY_IF_SHORTER(LIST_B, 6, true, false);
+    bool b7 = LIBFLATARRAY_IF_SHORTER(LIST_B, 7, true, false);
+    bool b8 = LIBFLATARRAY_IF_SHORTER(LIST_B, 8, true, false);
+    bool b9 = LIBFLATARRAY_IF_SHORTER(LIST_B, 9, true, false);
+
+    bool c0 = LIBFLATARRAY_IF_SHORTER(LIST_C, 0, false, true);
+    bool c1 = LIBFLATARRAY_IF_SHORTER(LIST_C, 1, false, true);
+    bool c2 = LIBFLATARRAY_IF_SHORTER(LIST_C, 2, true, false);
+    bool c3 = LIBFLATARRAY_IF_SHORTER(LIST_C, 3, true, false);
+    bool c4 = LIBFLATARRAY_IF_SHORTER(LIST_C, 4, true, false);
+
+    BOOST_TEST(a0);
+    BOOST_TEST(a1);
+    BOOST_TEST(a2);
+    BOOST_TEST(a3);
+    BOOST_TEST(a4);
+
+    BOOST_TEST(b0);
+    BOOST_TEST(b1);
+    BOOST_TEST(b2);
+    BOOST_TEST(b3);
+    BOOST_TEST(b4);
+    BOOST_TEST(b5);
+    BOOST_TEST(b6);
+    BOOST_TEST(b7);
+    BOOST_TEST(b8);
+    BOOST_TEST(b9);
+
+    BOOST_TEST(c0);
+    BOOST_TEST(c1);
+    BOOST_TEST(c2);
+    BOOST_TEST(c3);
+    BOOST_TEST(c4);
 }
 
 int main(int argc, char **argv)
