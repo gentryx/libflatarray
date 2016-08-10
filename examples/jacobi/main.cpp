970858a183b9124cfc3891bf1f7631a337ad4948
Andreas Schäfer <gentryx@gmx.de>
2016-08-10 10:49:39 +0200

    add C99 reference implementation for 3D Jacobi

--- examples/jacobi/main.cpp
+++ examples/jacobi/main.cpp
@@ -1,12 +1,102 @@
 /**
- * Copyright 2012-2013 Andreas Schäfer
+ * Copyright 2012-2016 Andreas Schäfer
  *
  * Distributed under the Boost Software License, Version 1.0. (See accompanying
  * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
+#include <cmath>
 #include <iostream>
+#include <sstream>
 #include <libflatarray/flat_array.hpp>
+#include <libflatarray/testbed/cpu_benchmark.hpp>
+#include <libflatarray/testbed/evaluate.hpp>
+
+extern "C" {
+    void update_c99(double *data_new, const double *data_old, int dim_x, int dim_y, int dim_z);
+}
+
+class benchmark_c_style_3d_jacobi : public LibFlatArray::cpu_benchmark
+{
+public:
+    std::string family()
+    {
+        return "Jacobi3D";
+    }
+
+    std::string species()
+    {
+        return "C99";
+    }
+
+    std::string unit()
+    {
+        return "GLUPS";
+    }
+
+    double performance(std::vector<int> dim)
+    {
+        std::size_t num_cells = std::size_t(dim[0]) * dim[1] * dim[2];
+        int max_steps = dim[3];
+
+        std::vector<double> grid_old(num_cells, 0);
+        std::vector<double> grid_new(num_cells, 0);
+        init(&grid_old, dim[0], dim[1], dim[2]);
+
+        double sum1 = sum(grid_old, dim[0], dim[1], dim[2]);
+        update_c99(grid_new.data(), grid_old.data(), dim[0], dim[1], dim[2]);
+        double sum2 = sum(grid_new, dim[0], dim[1], dim[2]);
+
+        double delta = std::fabs(sum1 - sum2);
+        if (delta > 0.1) {
+            throw std::logic_error("consistency check failed");
+        }
+
+        double time_start = time();
+
+        for (int t = 0; t < max_steps; ++t) {
+            update_c99(grid_new.data(), grid_old.data(), dim[0], dim[1], dim[2]);
+            using std::swap;
+            std::swap(grid_old, grid_new);
+        }
+
+        double time_end = time();
+        double time_total = time_end - time_start;
+
+        double lattice_updates = num_cells * max_steps;
+        double glups = lattice_updates * 1e-9 / time_total;
+        return glups;
+    }
+
+private:
+    void init(std::vector<double> *grid, std::size_t dim_x, std::size_t dim_y, std::size_t dim_z)
+    {
+        for (std::size_t z = 2; z < (dim_z - 2); ++z) {
+            for (std::size_t y = 2; y < (dim_y - 2); ++y) {
+                for (std::size_t x = 2; x < (dim_x - 2); ++x) {
+                    std::size_t index = z * dim_x * dim_y + y * dim_x + x;
+                    (*grid)[index] = 1;
+                }
+            }
+        }
+    }
+
+    double sum(const std::vector<double> grid, std::size_t dim_x, std::size_t dim_y, std::size_t dim_z)
+    {
+        double sum = 0;
+
+        for (std::size_t z = 1; z < (dim_z - 1); ++z) {
+            for (std::size_t y = 1; y < (dim_y - 1); ++y) {
+                for (std::size_t x = 1; x < (dim_x - 1); ++x) {
+                    std::size_t index = z * dim_x * dim_y + y * dim_x + x;
+                    sum += grid[index];
+                }
+            }
+        }
+
+        return sum;
+    }
+};
 
 class Cell
 {
@@ -24,6 +114,7 @@ public:
     {}
 
     // fixme: write something sensible here
+    // fixme: goal: utilize non-temporal stores
     template<typename NEIGHBORHOOD>
     void update(const NEIGHBORHOOD& hood)
     {
@@ -36,11 +127,56 @@ private:
 
 LIBFLATARRAY_REGISTER_SOA(Cell, ((double)(temp)))
 
+void print_usage(const std::string& program_name)
+{
+    std::cerr << "USAGE: " << program_name << " [DIM_X] [DIM_Y] [DIM_Z] [STEPS]" << std::endl;
+}
+
 int main(int argc, char **argv)
 {
-    std::cout << "go\n";
+    if (argc > 5) {
+        print_usage(argv[0]);
+        return 1;
+    }
 
-    std::cout << "boomer " << LibFlatArray::detail::flat_array::offset<Cell, 1>::OFFSET << "\n";
+    std::stringstream buf;
+    for (int i = 1; i < argc; ++i) {
+        buf << argv[i] << " ";
+    }
+
+    int dim_x = 512;
+    int dim_y = 512;
+    int dim_z = 512;
+    int steps = 10;
+
+    if (argc >= 2) {
+        buf >> dim_x;
+    }
+    if (argc >= 3) {
+        buf >> dim_y;
+    }
+    if (argc >= 4) {
+        buf >> dim_z;
+    }
+    if (argc >= 5) {
+        buf >> steps;
+    }
+
+    if ((dim_x * dim_y * dim_z * steps) == 0) {
+        print_usage(argv[0]);
+        return 2;
+    }
+
+    std::vector<int> dim;
+    dim.push_back(dim_x);
+    dim.push_back(dim_y);
+    dim.push_back(dim_z);
+    dim.push_back(steps);
+
+    benchmark_c_style_3d_jacobi benchmark;
+    LibFlatArray::evaluate eval("", "none");
+    eval.print_header();
+    eval(benchmark, dim, true);
 
     return 0;
 }
---
+++ examples/jacobi/update_c99.c
@@ -0,0 +1,28 @@
+#include <omp.h>
+
+/**
+ * Recommended reference for multi-dimensional array handling in C99
+ * by Jeff Hammond:
+ *
+ *   https://github.com/jeffhammond/HPCInfo/blob/master/c99/array3d.c
+ */
+void update_c99(double *data_new, const double *data_old, int dim_x, int dim_y, int dim_z)
+{
+    double (* const restrict grid_old)[dim_y][dim_x] = (double (* const)[dim_y][dim_x])data_old;
+    double (*       restrict grid_new)[dim_y][dim_x] = (double (*      )[dim_y][dim_x])data_new;
+
+#pragma omp parallel for schedule(static)
+    for (int z = 1; z < (dim_z - 1); ++z) {
+        for (int y = 1; y < (dim_y - 1); ++y) {
+            for (int x = 1; x < (dim_x - 1); ++x) {
+                grid_new[z][y][x] =
+                    (grid_old[z - 1][y    ][x    ] +
+                     grid_old[z    ][y - 1][x    ] +
+                     grid_old[z    ][y    ][x - 1] +
+                     grid_old[z    ][y    ][x + 1] +
+                     grid_old[z    ][y + 1][x    ] +
+                     grid_old[z + 1][y    ][x    ]) * (1.0 / 6.0);
+            }
+        }
+    }
+}
