715d4f54b52549337c0694eb5d44d3c6b180ca6c
Andreas Schäfer <gentryx@gmx.de>
2016-08-11 09:45:00 +0200

    add (empty) test for macros header

---
+++ test/macros_test.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2016 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <libflatarray/macros.hpp>
+
+#include "test.hpp"
+
+ADD_TEST(TestLoopPeeler)
+{
+    std::cout << "foo bar\n";
+}
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
