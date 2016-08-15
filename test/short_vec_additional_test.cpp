37a03dcbaed726626ee6f4e90332473eceff3c3c
Andreas Schäfer <gentryx@gmx.de>
2016-08-15 07:13:11 +0200

    add doc

--- test/short_vec_additional_test.cpp
+++ test/short_vec_additional_test.cpp
@@ -5,4 +5,6 @@
  * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
+// include here to have another object file and check if linking with
+// origninal test still works.
 #include <libflatarray/short_vec.hpp>
