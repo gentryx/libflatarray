17f9d6f53311c56b38dabbe85f85590c760c6fdf
Andreas Schäfer <gentryx@gmx.de>
2016-08-26 01:13:32 +0200

    add missing boolean reductions

--- src/detail/short_vec_avx_double_4.hpp
+++ src/detail/short_vec_avx_double_4.hpp
@@ -70,6 +70,22 @@ public:
 #endif
 
     inline
+    bool any() const
+    {
+        // merge both 128-bit lanes of AVX register:
+        __m128d buf1 = _mm_or_pd(
+            _mm256_extractf128_pd(val1, 0),
+            _mm256_extractf128_pd(val1, 1));
+        // shuffle upper 64-bit half down to first 64 bits so we can
+        // "or" both together:
+        __m128d buf2 = _mm_shuffle_pd(buf1, buf1, 1 << 0);
+        buf2 = _mm_or_pd(buf1, buf2);
+        // another shuffle to extract the upper 64-bit half:
+        buf1 = _mm_shuffle_pd(buf2, buf2, 1 << 0);
+        return _mm_cvtsd_f64(buf1) || _mm_cvtsd_f64(buf2);
+    }
+
+    inline
     void operator-=(const short_vec<double, 4>& other)
     {
         val1 = _mm256_sub_pd(val1, other.val1);
