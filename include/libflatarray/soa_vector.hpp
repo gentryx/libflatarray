8f9287c483e2464c97aae7a63de02933f9daea85
Andreas Schäfer <gentryx@gmx.de>
2016-10-22 00:52:08 +0200

    test soa_vector c-tors

--- include/libflatarray/soa_vector.hpp
+++ include/libflatarray/soa_vector.hpp
@@ -28,28 +28,63 @@ public:
     inline
     __host__ __device__
     explicit soa_vector(std::size_t count = 0) :
-        grid(elements, 1, 1),
+        grid(count, 1, 1),
         count(count)
     {}
 
     inline
     __host__ __device__
     explicit soa_vector(std::size_t count, const value_type& value) :
-        grid(count, 1, 1)
+        grid(count, 1, 1),
+        count(count)
     {
-        // fixme: copy in default value
+        grid.broadcast(0, 0, 0, value, count);
+    }
+
+    /**
+     * Copies an element to the given index. We're intentionally not
+     * using at() or operator[] to avoid mismatched expectations here:
+     * we can't yield references to a T here.
+     */
+    inline
+    __host__ __device__
+    void set(std::size_t index, const T& element)
+    {
+        grid.set(index, 0, 0, element);
+    }
+
+    /**
+     * Copy out an element. Again we're not using at() or operator[]
+     * here to avoid confusion with the API: we can't return
+     * references from an SoA container.
+     */
+    inline
+    __host__ __device__
+    T get(std::size_t index) const
+    {
+        return grid.get(index, 0, 0);
     }
 
     inline
     __host__ __device__
-    std::size_t size()
+    std::size_t size() const
     {
         return count;
     }
+
+    inline
+    __host__ __device__
+    void clear()
+    {
+        count = 0;
+    }
+
 private:
     soa_grid<T> grid;
     std::size_t count;
 
+    // fixme: retrieval of multiple elements
+    // fixme: emplace
     // fixme: at()
     // fixme: operator[]
     // fixme: resize
--- test/soa_vector_test.cpp
+++ test/soa_vector_test.cpp
@@ -13,20 +13,52 @@ class Particle
 {
 public:
     float pos[3];
-    float velocity[3];
+    float vel[3];
     int time_to_live;
 };
 
 LIBFLATARRAY_REGISTER_SOA(
     Particle,
     ((float)(pos)(3))
-    ((float)(velocity)(3))
+    ((float)(vel)(3))
     ((int)(time_to_live)))
 
 namespace LibFlatArray {
 
 ADD_TEST(TestConstructor)
 {
+    Particle reference;
+    reference.pos[0] = 1.0f;
+    reference.pos[1] = 2.0f;
+    reference.pos[2] = 3.0f;
+    reference.vel[0] = 4.0f;
+    reference.vel[1] = 5.0f;
+    reference.vel[2] = 6.0f;
+    reference.time_to_live = 1234;
+
+    soa_vector<Particle> vec1;
+    soa_vector<Particle> vec2(20, reference);
+
+    BOOST_TEST_EQ(vec1.size(),  0);
+    BOOST_TEST_EQ(vec2.size(), 20);
+
+    vec1 = vec2;
+    BOOST_TEST_EQ(vec1.size(), 20);
+    BOOST_TEST_EQ(vec2.size(), 20);
+
+    vec2.clear();
+    BOOST_TEST_EQ(vec1.size(), 20);
+    BOOST_TEST_EQ(vec2.size(),  0);
+
+    for (std::size_t i = 0; i < vec1.size(); ++i) {
+        BOOST_TEST_EQ(1.0f, vec1.get(i).pos[0]);
+        BOOST_TEST_EQ(2.0f, vec1.get(i).pos[1]);
+        BOOST_TEST_EQ(3.0f, vec1.get(i).pos[2]);
+        BOOST_TEST_EQ(4.0f, vec1.get(i).vel[0]);
+        BOOST_TEST_EQ(5.0f, vec1.get(i).vel[1]);
+        BOOST_TEST_EQ(6.0f, vec1.get(i).vel[2]);
+        BOOST_TEST_EQ(1234, vec1.get(i).time_to_live);
+    }
 }
 
 }
