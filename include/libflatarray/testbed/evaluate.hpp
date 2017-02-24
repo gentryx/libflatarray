f10a90e8ecfb6cfa054d8006f7d75352f5378ef3
Andreas Schäfer <gentryx@gmx.de>
2017-02-24 07:45:02 +0100

    update copyright notice

--- include/libflatarray/testbed/evaluate.hpp
+++ include/libflatarray/testbed/evaluate.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2016 Andreas Schäfer
+ * Copyright 2014-2017 Andreas Schäfer
  *
  * Distributed under the Boost Software License, Version 1.0. (See accompanying
  * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
