2f2c1c886727411e2a45a00793809b7e7a166177
Andreas Schäfer <gentryx@gmx.de>
2017-02-28 23:36:43 +0100

    MSVC odyssey, step 61

--- include/libflatarray/detail/macros.hpp
+++ include/libflatarray/detail/macros.hpp
@@ -40,12 +40,7 @@
                 MEMBER,                                                 \
                 1,                                                      \
                 LIBFLATARRAY_ARRAY_ARITY(MEMBER));                      \
-    };                                                                  \
-    }                                                                   \
-    }
-
-/*
-                                                \
+                                                                        \
         template<typename MEMBER_TYPE>                                  \
         inline                                                          \
         int operator()(MEMBER_TYPE CELL_TYPE:: *member_ptr)             \
@@ -78,8 +73,6 @@
     };                                                                  \
     }                                                                   \
     }
-*/
-
 
 #define LIBFLATARRAY_DECLARE_SOA_MEMBER(MEMBER_INDEX, CELL, MEMBER, CONST, INDEX_VAR) \
     LIBFLATARRAY_ARRAY_CONDITIONAL(MEMBER, , template<int ARRAY_INDEX >) \
