4fcad4720447b6ff028072dadb279195188a99e1
Andreas Schäfer <gentryx@gmx.de>
2017-02-28 22:57:05 +0100

    MSVC odyssey, step 56

--- test/api_traits_test.cpp
+++ test/api_traits_test.cpp
@@ -677,8 +677,8 @@ LIBFLATARRAY_REGISTER_SOA(CellDefaultSizes,          ((double)(memberA))((double
 #define ANDI0(x) ANDIM1(x)
 #define ANDIM1(x) LIBFLATARRAY_DEREF_ ## x
 // #define ANDI1(x) BOOST_PP_SEQ_SIZE((double)(memberA))
-#define ANDI2(x) FF ## ANDI3(x)
-#define ANDI3(x) ANDI4(x)
+#define ANDI2(x) ANDI3(x)
+#define ANDI3(x) FF ## ANDI4(x)
 #define ANDI4(x) #x
 #pragma message( "andi42")
 #pragma message( ANDI2(ANDI1(SEQ)) )
