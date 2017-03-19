ea88cca013c9b80e9780d5b5f1beb846fccb0c18
Andreas Schäfer <gentryx@gmx.de>
2017-03-19 16:34:43 +0100

    remove another warning that MSVC is complaining about from time to time

--- include/libflatarray/macros.hpp
+++ include/libflatarray/macros.hpp
@@ -57,7 +57,7 @@
  */
 #  define LIBFLATARRAY_REGISTER_SOA(CELL_TYPE, CELL_MEMBERS)    \
     __pragma( warning( push ) )                                 \
-    __pragma( warning( disable : 4307 4514 ) )                  \
+    __pragma( warning( disable : 4307 4514 4626 ) )             \
     LIBFLATARRAY_REGISTER_SOA_MAIN(CELL_TYPE, CELL_MEMBERS)     \
     __pragma( warning( pop ) )                                  \
 
