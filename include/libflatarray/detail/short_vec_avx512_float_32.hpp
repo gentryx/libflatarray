6fc22b5d475f649591d32ce09a9351a0f299b52b
Andreas Schäfer <gentryx@gmx.de>
2017-08-11 08:26:04 +0200

    add missing copyright marks

--- include/libflatarray/detail/short_vec_avx512_float_32.hpp
+++ include/libflatarray/detail/short_vec_avx512_float_32.hpp
@@ -1,6 +1,7 @@
 /**
  * Copyright 2015 Kurt Kanzenbach
  * Copyright 2016-2017 Andreas Schäfer
+ * Copyright 2017 Google
  *
  * Distributed under the Boost Software License, Version 1.0. (See accompanying
  * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
--- include/libflatarray/soa_grid.hpp
+++ include/libflatarray/soa_grid.hpp
@@ -1,5 +1,6 @@
 /**
  * Copyright 2014-2017 Andreas Schäfer
+ * Copyright 2017 Google
  *
  * Distributed under the Boost Software License, Version 1.0. (See accompanying
  * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
