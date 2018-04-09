d0b1b6e588798a4ac4ff44640c59e83c42cd716f
Andreas Schäfer <gentryx@gmx.de>
2018-04-09 20:03:06 +0200

    debug origin of warnings in performance tests example, 31/infty

--- include/libflatarray/testbed/benchmark.hpp
+++ include/libflatarray/testbed/benchmark.hpp
@@ -9,24 +9,24 @@
 #ifndef FLAT_ARRAY_TESTBED_BENCHMARK_HPP
 #define FLAT_ARRAY_TESTBED_BENCHMARK_HPP
 
-// // disable certain warnings from system headers when compiling with
-// // Microsoft Visual Studio:
-// #ifdef _MSC_BUILD
-// #pragma warning( push )
-// #pragma warning( disable : 4514 4668 4710 4820 )
-// #endif
+// disable certain warnings from system headers when compiling with
+// Microsoft Visual Studio:
+#ifdef _MSC_BUILD
+#pragma warning( push )
+#pragma warning( disable : 4514 4668 4710 4820 )
+#endif
 
-// #include <vector>
+#include <vector>
 
-// #ifdef _WIN32
-// #include <windows.h>
-// #else
-// #include <sys/time.h>
-// #endif
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
 
-// #ifdef _MSC_BUILD
-// #pragma warning( pop )
-// #endif
+#ifdef _MSC_BUILD
+#pragma warning( pop )
+#endif
 
 namespace LibFlatArray {
 
@@ -46,17 +46,17 @@ public:
     static
     inline double time()
     {
-// #ifdef _WIN32
-//         LARGE_INTEGER time;
-//         LARGE_INTEGER freq;
-//         QueryPerformanceCounter(&time);
-//         QueryPerformanceFrequency(&freq);
-//         return 1.0 * time.QuadPart / freq.QuadPart;
-// #else
-//         timeval t;
-//         gettimeofday(&t, 0);
-//         return t.tv_sec + t.tv_usec * 1.0e-6;
-// #endif
+#ifdef _WIN32
+        LARGE_INTEGER time;
+        LARGE_INTEGER freq;
+        QueryPerformanceCounter(&time);
+        QueryPerformanceFrequency(&freq);
+        return 1.0 * time.QuadPart / freq.QuadPart;
+#else
+        timeval t;
+        gettimeofday(&t, 0);
+        return t.tv_sec + t.tv_usec * 1.0e-6;
+#endif
         return 0;
     }
 
