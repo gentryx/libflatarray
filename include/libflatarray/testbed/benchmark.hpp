74e61601c8b504f9c52057ab07ed4516349a4d89
Andreas Schäfer <gentryx@gmx.de>
2018-04-09 18:44:10 +0200

    debug origin of warnings in performance tests example, 23/infty

--- include/libflatarray/testbed/benchmark.hpp
+++ include/libflatarray/testbed/benchmark.hpp
@@ -9,24 +9,24 @@
 #ifndef FLAT_ARRAY_TESTBED_BENCHMARK_HPP
 #define FLAT_ARRAY_TESTBED_BENCHMARK_HPP
 
-// disable certain warnings from system headers when compiling with
-// Microsoft Visual Studio:
-#ifdef _MSC_BUILD
-#pragma warning( push )
-#pragma warning( disable : 4514 4668 4710 4820 )
-#endif
+// // disable certain warnings from system headers when compiling with
+// // Microsoft Visual Studio:
+// #ifdef _MSC_BUILD
+// #pragma warning( push )
+// #pragma warning( disable : 4514 4668 4710 4820 )
+// #endif
 
-#include <vector>
+// #include <vector>
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <sys/time.h>
-#endif
+// #ifdef _WIN32
+// #include <windows.h>
+// #else
+// #include <sys/time.h>
+// #endif
 
-#ifdef _MSC_BUILD
-#pragma warning( pop )
-#endif
+// #ifdef _MSC_BUILD
+// #pragma warning( pop )
+// #endif
 
 namespace LibFlatArray {
 
