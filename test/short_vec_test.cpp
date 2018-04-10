5e7d7e61b020e961ac17d2673e2199f6e8b0f485
Andreas Schäfer <gentryx@gmx.de>
2018-04-10 13:27:30 +0200

    less warnings in tests, take 17

--- test/short_vec_test.cpp
+++ test/short_vec_test.cpp
@@ -10,8 +10,8 @@
 
 #include <libflatarray/config.h>
 #include <libflatarray/aligned_allocator.hpp>
-// #include <libflatarray/macros.hpp>
-// #include <libflatarray/short_vec.hpp>
+#include <libflatarray/macros.hpp>
+#include <libflatarray/short_vec.hpp>
 
 // globally disable some warnings with MSVC, that are issued not for a
 // specific header, but rather for the interaction of system headers
@@ -48,17 +48,16 @@ namespace LibFlatArray {
 template<typename CARGO, std::size_t ARITY>
 void testImplementationReal()
 {
-    std::size_t numElements = 4;
-    // typedef SHORT_VEC_TEMPLATE<CARGO, ARITY> ShortVec;
-    // std::size_t numElements = ShortVec::ARITY * 5;
+    typedef SHORT_VEC_TEMPLATE<CARGO, ARITY> ShortVec;
+    std::size_t numElements = ShortVec::ARITY * 5;
 
     std::vector<CARGO, aligned_allocator<CARGO, 64> > vec1(numElements);
     std::vector<CARGO, aligned_allocator<CARGO, 64> > vec2(numElements, 4711);
 
-    // // init vec1:
-    // for (std::size_t i = 0; i < numElements; ++i) {
-    //     vec1[i] = i + 0.1;
-    // }
+    // init vec1:
+    for (std::size_t i = 0; i < numElements; ++i) {
+        vec1[i] = i + 0.1;
+    }
 
     // test size:
     {
