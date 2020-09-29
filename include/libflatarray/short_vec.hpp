432376279cb03c427871acf63c013b567f6b8b25
Andreas Schäfer <gentryx@gmx.de>
2020-09-30 01:26:32 +0200

    still fiddling around, part 5

--- include/libflatarray/short_vec.hpp
+++ include/libflatarray/short_vec.hpp
@@ -306,16 +306,16 @@ public:
 // This ugly conditional is required for Travis CI, where some VMs
 // define __AVX512F__ despite running an ancient GCC 5.
 #pragma message "Travis CI test A"
-#      if defined( __AVX512F__) && (!defined(__GNUCC__) || (__GNUCC__ >= 7))
 #pragma message "Travis CI test B: " __AVX512F__ " blah"
 #pragma message "Travis CI test C: " __GNUCC__ " blah"
 #define XSTR(x) STR(x)
 #define STR(x) #x
 #ifdef(__GNUCC__)
-#pragma message ("Travis CI test D1: " XSTR(__GNUCC__) " blah")
+#pragma message ("Travis CI test D1: " XSTR(__GNUC__) " blah")
 #endif
-#pragma message ("Travis CI test D2: " XSTR(__GNUCC__) " blah")
+#pragma message ("Travis CI test D2: " XSTR(__GNUC__) " blah")
 #pragma message ("Travis CI test E: " XSTR(__AVX512F__) " blah")
+#      if defined( __AVX512F__) && (!defined(__GNUC__) || (__GNUC__ >= 7))
 #        define LIBFLATARRAY_WIDEST_VECTOR_ISA LIBFLATARRAY_AVX512F
 #      else
 #        ifdef __AVX2__
